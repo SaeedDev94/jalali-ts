383574d108ed51221abae287cb4accd2791c4480
SaeedPooyanfar <saeedp47@gmail.com>
2022-06-12 23:58:31 +0430

    modify jalali.test.mjs

--- jalali.test.mjs
+++ jalali.test.mjs
@@ -15,46 +15,36 @@ describe('Jalali', () => {
       const dateFormat = 'YYYY/MM/DD';
       const dateTimeFormat = `${dateFormat} HH:mm:ss`;
       const jalali = Jalali.parse('1398/12/04');
-      it('Same timestamp: 1582403400000', () => assert.equal(
-        +jalali,
-        1582403400000
-      ));
-      it("Same gregorian: '2020/02/23'", () => assert.equal(
-        jalali.format(dateFormat, true),
-        '2020/02/23'
-      ));
-      it("Is leap year: false", () => assert.equal(
-        jalali.isLeapYear(),
-        false
-      ));
-      it('Month length: 29', () => assert.equal(
-        jalali.monthLength(),
-        29
-      ));
-      it("Add 2 month + 1 week: '1399/02/11'", () => assert.equal(
-        jalali.add(2, 'month').add(1, 'week').format(dateFormat),
-        '1399/02/11'
-      ));
-      it("Start of week: '1399/02/06 00:00:00'", () => assert.equal(
-        jalali.startOf('week').format(dateTimeFormat),
-        '1399/02/06 00:00:00'
-      ));
-      it('Day of year: 37', () => assert.equal(
-        jalali.dayOfYear(),
-        37
-      ));
-      it("End of year: '1399/12/30 23:59:59'", () => assert.equal(
-        jalali.endOf('year').format(dateTimeFormat),
-        '1399/12/30 23:59:59'
-      ));
-      it("Is leap year: true", () => assert.equal(
-        jalali.isLeapYear(),
-        true
-      ));
-      it("Add 1 day + start of day: '1400-01-01 12:00:00 AM'", () => assert.equal(
-        jalali.add(1, 'day').startOf('day').format('YYYY-MM-DD hh:mm:ss A'),
-        '1400-01-01 12:00:00 AM'
-      ));
+
+      const timestamp = 1582403400000;
+      it(`Same timestamp: ${timestamp}`, () => assert.equal(+jalali, timestamp));
+
+      const gregorian = '2020/02/23';
+      it(`Same gregorian: '${gregorian}'`, () => assert.equal(jalali.format(dateFormat, true), gregorian));
+
+      const is1398LeapYear = false;
+      it(`Is leap year: ${is1398LeapYear}`, () => assert.equal(jalali.isLeapYear(), is1398LeapYear));
+
+      const monthLength = 29;
+      it(`Month length: ${monthLength}`, () => assert.equal(jalali.monthLength(), 29));
+
+      const add2MonthAnd1Week = '1399/02/11';
+      it(`Add 2 month + 1 week: '${add2MonthAnd1Week}'`, () => assert.equal(jalali.add(2, 'month').add(1, 'week').format(dateFormat), add2MonthAnd1Week));
+
+      const startOfWeek = '1399/02/06 00:00:00';
+      it(`Start of week: '${startOfWeek}'`, () => assert.equal(jalali.startOf('week').format(dateTimeFormat), startOfWeek));
+
+      const dayOfYear = 37;
+      it(`Day of year: ${dayOfYear}`, () => assert.equal(jalali.dayOfYear(), dayOfYear));
+
+      const endOfYear = '1399/12/30 23:59:59';
+      it(`End of year: '${endOfYear}'`, () => assert.equal(jalali.endOf('year').format(dateTimeFormat), endOfYear));
+
+      const is1399LeapYear = true;
+      it(`Is leap year: ${is1398LeapYear}`, () => assert.equal(jalali.isLeapYear(), is1399LeapYear));
+
+      const add1DayAndStartOfDay = '1400-01-01 12:00:00 AM';
+      it(`Add 1 day + start of day: '${add1DayAndStartOfDay}'`, () => assert.equal(jalali.add(1, 'day').startOf('day').format('YYYY-MM-DD hh:mm:ss A'), add1DayAndStartOfDay));
     });
 
   });

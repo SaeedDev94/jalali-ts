06331f2e67e50f6b794dc62fcd07f986746a3bf3
SaeedPooyanfar <saeedp47@gmail.com>
2022-06-20 15:41:10 +0430

    test: DateTime

--- test.mjs
+++ test.mjs
@@ -8,23 +8,14 @@ describe('Jalali', () => {
     describe('Pass JS native date object to Jalali constructor', () => {
       const date = new Date();
       const jalali = new Jalali(date);
-      it(
-        '+jalali === +date // true',
-        () => assert.equal(+jalali === +date, true)
-      );
+      it('+jalali === +date // true', () => assert.equal(+jalali === +date, true));
     });
 
     describe('Clone jalali', () => {
       const now = Jalali.now();
       const clone = now.clone();
-      it(
-        "Shouldn't be same with its clone",
-        () => assert.equal(now === clone, false)
-      );
-      it(
-        'Should has same value',
-        () => assert.equal(+now === +clone, true)
-      );
+      it("Shouldn't be same with its clone", () => assert.equal(now === clone, false));
+      it('Should has same value', () => assert.equal(+now === +clone, true));
     });
 
     describe("Parse date: '1398/12/04'", () => {
@@ -95,6 +86,25 @@ describe('Jalali', () => {
         `#add(1, 'day').startOf('day'): '${add1DayAndStartOfDay}'`,
         () => assert.equal(`${jalali.add(1, 'day').startOf('day')}`, add1DayAndStartOfDay)
       );
+    });
+
+    describe(`DateTime: '1398/12/04 02:30:07:05 PM'`, () => {
+      const dateTime = Jalali.parse('1398/12/04 02:30:07:05 PM');
+
+      it('#valueOf', () => assert.equal(+dateTime, 1582455607050));
+      it('#getHours', () => assert.equal(dateTime.getHours(), 14));
+      it('#getMinutes', () => assert.equal(dateTime.getMinutes(), 30));
+      it('#getSeconds', () => assert.equal(dateTime.getSeconds(), 7));
+      it('#getMilliseconds', () => assert.equal(dateTime.getMilliseconds(), 50));
+      it('#gregorian', () => assert.equal(dateTime.gregorian('YYYY-MM-DD HH:mm:ss.SSS'), '2020-02-23 14:30:07.050'));
+    });
+
+    describe(`DateTime (No milliseconds): '1398/12/04 02:30:07:05 PM'`, () => {
+      const dateTime = Jalali.parse('1398/12/04 02:30:07:05 PM', false);
+
+      it('#valueOf', () => assert.equal(+dateTime, 1582455607000));
+      it('#getMilliseconds', () => assert.equal(dateTime.getMilliseconds(), 0));
+      it('#gregorian', () => assert.equal(dateTime.gregorian('YYYY-MM-DD HH:mm:ss.SSS'), '2020-02-23 14:30:07.000'));
     });
 
   });
